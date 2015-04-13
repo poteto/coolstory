a6702f1d499ab11b745c76fc1b958ed5af452084
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-04-13 15:04:22 -0400

    installing materialize will break the build

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('bower_components/materialize/bin/materialize.js');
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
