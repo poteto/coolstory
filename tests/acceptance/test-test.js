c879b2a77c257dff521c8d2d608a5262f3f15569
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-04-13 14:42:59 -0400

    installed ember-cli-flash as a devDependency

---
+++ tests/acceptance/test-test.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import {
+  module,
+  test
+} from 'qunit';
+import startApp from 'coolstory/tests/helpers/start-app';
+
+var application;
+
+module('Acceptance: Test', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
